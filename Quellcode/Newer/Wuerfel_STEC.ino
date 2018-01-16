8ee4fd225cdf79a152aedbf6ef95dd6aef7169de
M1ssingLink <35104343+M1ssingLink@users.noreply.github.com>
2018-01-16 23:01:26 +0100

    Update
    Update

---
+++ Quellcode/Newer/Wuerfel_STEC.ino
@@ -0,0 +1,56 @@
+/*
+ * Simple d6 visualizer via the use of 7 LED´s
+ * and a single Button which is declaring the intention to roll.
+ */
+
+#include "initialize.h"
+
+
+
+int buttonState = 0;
+int LEDTestConsecutive = 0;
+int LEDTestSimultaneous = 0;
+int rollTestState = 0;
+
+
+/*
+ * Initializer for the LED´s, and the Button
+ */
+void setup() {
+    Serial.begin(9600);
+    initializer();
+    startAnimation();
+    
+}
+
+/*
+ * Every second the input of the Button-input is read
+ * and if pressed the random d6 result gets displayed
+ */
+void loop() {
+  
+ 
+      buttonState = digitalRead(triggerButton);
+
+    if (buttonState == HIGH) {
+        Serial.println("button pressed");
+        d6Roller(1, 7);
+        delay(1000);
+        reset();
+
+    }
+    
+    /* Tests if enabled */
+    if (LEDTestConsecutive == 1) {
+        LEDTestConsecutively();
+    }
+    if (LEDTestSimultaneous == 1) {
+        LEDTestSimultaneously();
+    }
+    if (rollTestState == 1){
+      rollTester(1, 7);
+    }
+}
+    
+    
+   
